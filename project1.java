6333f63ac436428d4289779cb41429ae8c70d784
idrA <marcgeminiano43@gmail.com>
2025-07-26 16:22:54 -0700

    Create project1.java

---
+++ project1.java
@@ -0,0 +1,16 @@
+// Simple operations
+public class Main {
+    public static void main(String[] args) {
+      int a = 80,
+          b = 80,
+          c = 80,
+          d = 80,
+          e = 80,
+          f = 80,
+          g = 80,
+          h = 80;
+          int sum = a + b + c + d + e + f + g + h;
+        
+          System.out.println(sum / 8);
+    }
+}
